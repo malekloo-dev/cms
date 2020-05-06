cafe58509fbe4bf5787573a471b7ab797f30daa2
marjani.mahdi@gmail.com <mahdiMarjani115081>
2020-05-06 16:23:36 +0430

    v0.0.1 - fixed home design

--- resources/views/tarhoweb/Home.blade.php
+++ resources/views/tarhoweb/Home.blade.php
@@ -9,13 +9,13 @@
 
 <section id="index-intro">
 
-    <div class="flex one two-700 two-900">
+    <div class="flex one two-700 two-900 center">
         <div class="align-left">
-            <img style="width: 100%" src="{{ asset(@env('TEMPLATE_NAME').'/img/user/banner2.jpg') }}">
+
         </div>
         <div class="index-h1">
             <h1>مطالب کاربردی سئو</h1>
-            <h3>… به دنیای شگفت انگیز وب خوش آمدید</h3>
+            <h3>به دنیای شگفت انگیز وب خوش آمدید </h3>
             <a class="button button-blue">برای شروع کلیک کنید ...</a>
         </div>
     </div>
@@ -63,32 +63,7 @@
 <section class=" pb-5 m-0" style="position: relative">
     <div class="flex one">
         <div>
-            <h2 class="align-center">نظرات شما</h2>
-            <div class="quotes mt-2 flex one  three-700 four-1000 center ">
-                <div class="">
-                    <div class="box box1">
-                        <p>این دستگاه از تکنولوژی بالایی برخوردار است خصوصا دستگاه های مدل . داخل هد، برق کیلو ولت، های
-                            ولتاژ ساخته و به دو سر لامپ رفته و اشعه ایکس بوجود میآید. به همین دلیل کوچکترین </p>
-                        <h2>مهدی مرجانی</h2>
-                    </div>
-                    <div class="bg"></div>
-                </div>
-                <div class="">
-                    <div class="box box2">
-                        <p>بهترین نمونه کارها را دارند. بهترین نمونه کارها را دارند.</p>
-                        <h2>محمود ملک لو</h2>
-                    </div>
-                    <div class="bg"></div>
-                </div>
-                <div class="">
-                    <div class="box box3">
-                        <p>های مدل . داخل هد، برق کیلو ولت، های ولتاژ ساخته و به دو سر لامپ رفته و اشعه ایکس بوجود
-                            میآید.</p>
-                        <h2>شیلا خداداد</h2>
-                    </div>
-                    <div class="bg"></div>
-                </div>
-            </div>
+
         </div>
     </div>
 </section>
--- resources/views/tarhoweb/Nav.blade.php
+++ resources/views/tarhoweb/Nav.blade.php
@@ -3,9 +3,15 @@
     <a href="/" class="brand">
         <img class="full" src="{{ asset(@env('TEMPLATE_NAME').'/img/user/logo.png') }}">
     </a>
-    
+
     <input id="bmenu" type="checkbox" class="show">
-    <label for="bmenu" class="burger toggle pseudo button">منو</label>
+    <label for="bmenu" class="burger toggle pseudo button">
+        <span class="hamburger">
+            <span></span>
+            <span></span>
+            <span></span>
+        </span>
+    </label>
     <div class="menu">
         <a href="/" class="pseudo button">خانه</a>
         <a href="/ساخت-سوئیچ-ساخت-ریموت"  class=" pseudo button ">ساخت سوئیچ و ریموت</a>
