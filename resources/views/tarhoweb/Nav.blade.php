a2f6e33576e38d54ff4a61b1cf0237522f67c94a
marjani.mahdi@gmail.com <mahdiMarjani115081>
2020-05-07 16:35:19 +0430

    v0.0.1 - added multiple menu

--- resources/views/tarhoweb/Nav.blade.php
+++ resources/views/tarhoweb/Nav.blade.php
@@ -1,4 +1,3 @@
-
 <nav>
     <a href="/" class="brand">
         <img class="full" src="{{ asset(@env('TEMPLATE_NAME').'/img/logo.png') }}">
@@ -13,9 +12,33 @@
         </span>
     </label>
     <div class="menu">
-        <a href="/" class="pseudo button">خانه</a>
-        <a href="/ساخت-سوئیچ-ساخت-ریموت"  class=" pseudo button ">ساخت سوئیچ و ریموت</a>
-        <a href="/about-us" class="button pseudo">درباره ما</a>
-        <a href="/contact-us" class="button pseudo">تماس با ما</a>
+        <ul>
+            <li><a href="/">خانه</a></li>
+
+            <li class="parent"><a href="/ساخت-سوئیچ-ساخت-ریموت" >طراحی سایت</a>
+                <button><img src="{{ asset(@env('TEMPLATE_NAME').'/img/arrow-down.png') }}"></button>
+                <ul>
+                    <li><a href="/ساخت-سوئیچ-ساخت-ریموت" >طراحی سایت وردپرس</a></li>
+                    <li><a href="/ساخت-سوئیچ-ساخت-ریموت" >طراحی سایت حرفه ای</a></li>
+                    <li><a href="/ساخت-سوئیچ-ساخت-ریموت" >نمونه طراحی سایت</a></li>
+                </ul>
+            </li>
+            <li class="parent"><a href="/ساخت-سوئیچ-ساخت-ریموت" >سئو سایت</a>
+                <button><img src="{{ asset(@env('TEMPLATE_NAME').'/img/arrow-down.png') }}"></button>
+                <ul>
+                    <li><a href="/ساخت-سوئیچ-ساخت-ریموت" ">نمونه کار سئو</a></li>
+                </ul>
+            </li>
+            <li class="parent"><a href="/ساخت-سوئیچ-ساخت-ریموت" >تعرفه</a>
+                <button><img src="{{ asset(@env('TEMPLATE_NAME').'/img/arrow-down.png') }}"></button>
+                <ul>
+                    <li><a href="/ساخت-سوئیچ-ساخت-ریموت" >تعرفه سئو سایت</a></li>
+                    <li><a href="/ساخت-سوئیچ-ساخت-ریموت" >تعرفه طراحی سایت</a></li>
+                </ul>
+            </li>
+            <li><a href="/درباره-ما" >بلاگ</a></li>
+            <li><a href="/درباره-ما" >درباره ما</a></li>
+            <li><a href="/تماس-با-ما">تماس با ما</a></li>
+        </ul>
     </div>
 </nav>
