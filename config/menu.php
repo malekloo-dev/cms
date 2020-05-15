bc89500b5fcb89a1ec696b8c49f2daeb9dcc7a2d
malekloo <mahmoud.malekloo@gmail.com>
2020-05-15 18:11:13 +0430

    v0.0.1 change menu

--- config/menu.php
+++ config/menu.php
@@ -6,7 +6,7 @@ return [
    'middleware' => [],
 
 /* you can set your own table prefix here */
-    'table_prefix' => 'admin_',
+    'table_prefix' => '',
 
 /* you can set your own table names */
     'table_name_menus' => 'menus',
@@ -14,7 +14,7 @@ return [
     'table_name_items' => 'menu_items',
 
 /* you can set your route path*/
-    'route_path' => '/harimayco/',
+    'route_path' => '/menu/',
 
 /* here you can make menu items visible to specific roles */
     'use_roles' => false,
