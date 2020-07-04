7938c28562a25c10f65d10976f882c57abafd665
marjani.mahdi@gmail.com <mahdiMarjani115081>
2020-07-04 22:10:14 +0430

    add resume

--- resources/views/tarhoweb/cms/WebDesignResume.blade.php
+++ resources/views/tarhoweb/cms/WebDesignResume.blade.php
@@ -1,7 +1,7 @@
 @extends(@env('TEMPLATE_NAME').'.App')
 
 @section('assets')
-<link rel="stylesheet" type="text/css" href="{{ asset('/ProDesign.css') }}">
+<link rel="stylesheet" type="text/css" href="{{ asset('/WebDesignResume.css') }}">
 @endsection
 
 @section('Content')
@@ -18,30 +18,84 @@
     </div>
 </section>
 
-
-
-<section class="pro-design-intro m-0 pt-0">
-    <div class="flex one">
+<section class="custom-web-design mb-0 mt-3 ">
+    <div class="flex one two-700 three-900 center align-center">
         <div>
-            <div class="flex one two-700 two-900 center">
-                <div class="align-right web-design-title">
-                    <h1>طراحی سایت حرفه ای</h1>
-                    <h3>ما از طرح های جسورانه و کد نشانه گذاری فوق العاده تمیز برای تولید وب سایت هایی استفاده می کنیم که شگفت آور به نظر می رسند.
-                    </h3>
-                    <a class="button button-purple">تعرفه پکیج طراحی سایت حرفه ای</a>
-                </div>
-                <div class="index-h1">
-                    <img src="{{ asset('img/home/pc.png') }}">
-                </div>
-            </div>
-
+            <a href="{{url('/طراحی-سایت-حرفه-ای')}}">
+                <img src="{{asset('img/home/design.jpg')}}">
+                <h2>طراحی سایت حرفه ای</h2>
+            </a>
+        </div>
+        <div>
+            <a href="{{ url('/طراحی-سایت-وردپرس') }}">
+                <img src="{{asset('img/home/moshavere.jpg')}}">
+                <h2>طراحی سایت وردپرس</h2>
+            </a>
+        </div>
+        <div>
+            <a href="{{ url('/نمونه-طراحی-سایت') }}">
+                <img src="{{asset('img/home/seo.jpg')}}">
+                <h2>نمونه طراحی سایت</h2>
+            </a>
         </div>
     </div>
 
+
 </section>
 
+<svg viewbox="0 0 100 25">
+    <path fill="#5E16B0" opacity="0.5" d="M0 30 V15 Q30 3 60 15 V30z" />
+    <path fill="#5E16B0" d="M0 30 V12 Q30 17 55 12 T100 11 V30z" />
+</svg>
 
 
+<section class="resume mt-0 mb-0 pb-5">
+    <h2  class="align-center">نمونه کار</h2>
+    <div class="flex one two-500  four-900 center align-center">
+        <div>
+            <div class="gallery">
+                <div class="m-4">
+                    <img class="g1" src="{{asset('img/resume/var1.jpg')}}">
+                    <img class="g2" src="{{asset('img/resume/var2.jpg')}}">
+                    <img class="g3" src="{{asset('img/resume/var3.jpg')}}">
+                </div>
+            </div>
+            <h3>Various artist</h3>
+        </div>
+        <div>
+            <div class="gallery">
+                <div class="m-4">
+                    <img class="g1" src="{{asset('img/resume/var1.jpg')}}">
+                    <img class="g2" src="{{asset('img/resume/var2.jpg')}}">
+                    <img class="g3" src="{{asset('img/resume/var3.jpg')}}">
+                </div>
+            </div>
+            <h3>Bonzelfi</h3>
+        </div>
+        <div>
+            <div class="gallery">
+                <div class="m-4">
+                    <img class="g1" src="{{asset('img/resume/var1.jpg')}}">
+                    <img class="g2" src="{{asset('img/resume/var2.jpg')}}">
+                    <img class="g3" src="{{asset('img/resume/var3.jpg')}}">
+                </div>
+            </div>
+            <h3>Bonzelfi</h3>
+        </div>
+        <div>
+            <div class="gallery">
+                <div class="m-4">
+                    <img class="g1" src="{{asset('img/resume/var1.jpg')}}">
+                    <img class="g2" src="{{asset('img/resume/var2.jpg')}}">
+                    <img class="g3" src="{{asset('img/resume/var3.jpg')}}">
+                </div>
+            </div>
+            <h3>Bonzelfi</h3>
+        </div>
+
+
+    </div>
+</section>
 
 
 
