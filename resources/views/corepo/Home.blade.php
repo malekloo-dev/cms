4e8daf83854292811419db73303a64bb7f77fcea
marjani.mahdi@gmail.com <mahdiMarjani115081>
2020-09-05 12:10:15 +0430

    fixed corepo

--- resources/views/corepo/Home.blade.php
+++ resources/views/corepo/Home.blade.php
@@ -33,8 +33,53 @@
             dots[i].className = dots[i].className.replace(" active", "");
         }
         slides[slideIndex - 1].style.display = "block";
-        dots[slideIndex - 1].className += " active";
+        //dots[slideIndex - 1].className += " active";
     }
+</script>
+
+<script src="{{asset('/siema.min.js')}}"></script>
+<script>
+    var w;
+    var perPageNumber;
+
+    function perPage() {
+        w = window.innerWidth;
+        if (w <= 500) {
+            perPageNumber = 1;
+        } else if (w <= 768) {
+            perPageNumber = 3;
+        } else if (w <= 1024) {
+            perPageNumber = 5;
+        } else {
+            perPageNumber = 7;
+        }
+    }
+
+
+    document.getElementsByTagName("BODY")[0].onresize = function() {
+        mySiema.destroy();
+        perPage();
+        mySiema.init();
+    };
+
+
+    perPage();
+    var mySiema = new Siema({
+        selector: '.siema',
+        duration: 200,
+        easing: 'ease-out',
+        perPage: perPageNumber,
+        startIndex: 0,
+        draggable: true,
+        multipleDrag: true,
+        threshold: 20,
+        loop: false,
+        rtl: true,
+        onInit: () => {},
+        onChange: () => {},
+    });
+    document.querySelector('.prev2').addEventListener('click', () => mySiema.prev());
+    document.querySelector('.next2').addEventListener('click', () => mySiema.next());
 </script>
 @endsection
 
@@ -47,16 +92,16 @@
             <img class="banner-t shadow2" src="{{ asset('/img/bt.jpg') }}" srcset="">
             <img class="banner-b shadow2" src="{{ asset('/img/bb.jpg') }}" srcset="">
         </div>
-        <div class="two-third-500 slideshow-container pb-0 pl-1">
+        <div class="two-third-500 slideshow-container pb-0 pl-1 ">
 
             <div class="mySlides fade ">
-                <div class="numbertext">1 / 1</div>
+
                 <img class="shadow2" src="{{ asset('/img/main-banner.jpg') }}" style="width:100%">
                 <div class="text">طراحی سایت و سئو - طرح و وب</div>
             </div>
 
-            <div class="mySlides fade pt-1">
-                <div class="numbertext">1 / 1</div>
+            <div class="mySlides fade">
+
                 <img class="shadow2" src="{{ asset('/img/main-banner.jpg') }}" style="width:100%">
                 <div class="text">طراحی سایت و سئو - طرح و وب</div>
             </div>
@@ -75,21 +120,26 @@
 
 
 
-<section class="index-item-top  mt-0 mb-0 pt-5 pb-5 bg-white category-section">
-    <div class="flex one seven-500 center  ">
-        @foreach($category as $content)
-        <a href="{{ $content->slug }}">
-            <div class="hover text-center">
-                @if(isset($content->images['thumb']))
-                <img src="{{ $content->images['thumb'] }}" srcset="
+<section class="index-item-top  mt-0 mb-0 pt-5 pb-5 bg-white category-section" onresize="onResize()">
+    <div class="flex one center  ">
+        <div class="siema p-0">
+            @foreach($category as $content)
+            <a href="{{ $content->slug }}">
+                <div class="hover text-center">
+                    @if(isset($content->images['thumb']))
+                    <img src="{{ $content->images['thumb'] }}" srcset="
                 {{ $content->images['thumb'] }} ,
                 {{ $content->images['thumb'] }} 1.5x,
                 {{ $content->images['thumb'] }} 2x">
-                @endif
-                <h2 class="p-0 m-0 text-center"> {{ $content->title }}</h2>
-            </div>
-        </a>
-        @endforeach
+                    @endif
+                    <h2 class="p-0 m-0 text-center"> {{ $content->title }}</h2>
+                </div>
+            </a>
+            @endforeach
+
+        </div>
+        <a class="prev2">&#10094;</a>
+        <a class="next2">&#10095;</a>
     </div>
 </section>
 
