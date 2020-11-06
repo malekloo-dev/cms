c68832e27dcf5f1c15133e77df5cd1d4202748ab
mahdi <marjani@dabacenter.ir>
2020-11-06 15:47:29 +0330

    added manifst

--- resources/views/remotyadak/Head.blade.php
+++ resources/views/remotyadak/Head.blade.php
@@ -3,11 +3,14 @@
 
 <head>
     <title>{{$seo['meta_title'] ?? "" }}</title>
+    
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="keywords" content="{{$seo['meta_keywords'] ?? "" }}">
     <meta name="description" content="{{$seo['meta_description']  ?? ""}}">
-
+    
+    <link rel="manifest" href="{{ asset('/manifest.json') }}">
+    <meta name="theme-color" content="#fa490e"/>
     <link rel="stylesheet" href="{{ asset('/style.css') }}">
     <link rel="icon" href="{{ asset('/img/fav.png') }}" type="image/png">
     <link rel="stylesheet" media="bogus">
