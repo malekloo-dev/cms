1872b9f603f61a3d60575e0d9ca042726fab2468
malekloo <mahmoud.malekloo@gmail.com>
2021-04-24 15:28:48 +0430

    muticategory2

--- resources/views/remotyadak/Attr.blade.php
+++ resources/views/remotyadak/Attr.blade.php
@@ -1,14 +1,14 @@
 <div class="attr">
     <div class="">
-    @foreach($module['content'] as $key=>$attr)
-        <div class="">
-            <div >
-                {!!  $attr['field']!!}
+        @foreach($module['content'] as $key=>$attr)
+            <div class="">
+                <div >
+                    {!! isset($attr['field']) ? $attr['field'] :'' !!}
+                </div>
+                <div >
+                    {!! isset($attr['value']) ? $attr['value'] :'' !!}
+                </div>
             </div>
-            <div >
-                {!!  $attr['value']!!}
-            </div>
-        </div>
         @endforeach
     </div>
 </div>
