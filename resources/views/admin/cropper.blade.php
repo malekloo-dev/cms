2effa736b8bec52b1ce0657475940184bc16b0cf
mahdi <marjani.mahdi@gmail.com>
2021-05-19 18:40:47 +0430

    fixed

--- resources/views/admin/cropper.blade.php
+++ resources/views/admin/cropper.blade.php
@@ -17,8 +17,8 @@
         }
         .preview {
           overflow: hidden;
-          width: {{ env(Str::upper($content_info->attr_type).'_LARGE_W') }}px;
-          height: {{ env(Str::upper($content_info->attr_type).'_LARGE_H') }}px;
+          width: {{ env(Str::upper($content_info->attr_type??$attr_type).'_LARGE_W') }}px;
+          height: {{ env(Str::upper($content_info->attr_type??$attr_type).'_LARGE_H') }}px;
           margin: 10px;
           border: 1px solid red;
         }
@@ -91,7 +91,7 @@
         cropper = new Cropper(image, {
         //   aspectRatio: 1,
         //   viewMode: 3,
-          aspectRatio: {{ env(Str::upper($content_info->attr_type).'_SMALL_W') }}/{{ env(Str::upper($content_info->attr_type).'_SMALL_H') }},
+          aspectRatio: {{ env(Str::upper($content_info->attr_type??$attr_type).'_SMALL_W') }}/{{ env(Str::upper($content_info->attr_type??$attr_type).'_SMALL_H') }},
           viewMode: 0,
           preview: '.preview'
         });
@@ -103,8 +103,8 @@
     $("#crop").click(function(){
         canvas = cropper.getCroppedCanvas({
             fillColor:'#fff',
-            width: {{ env(Str::upper($content_info->attr_type).'_LARGE_W') }},
-            height: {{ env(Str::upper($content_info->attr_type).'_LARGE_H') }},
+            width: {{ env(Str::upper($content_info->attr_type??$attr_type).'_LARGE_W') }},
+            height: {{ env(Str::upper($content_info->attr_type??$attr_type).'_LARGE_H') }},
             imageSmoothingEnabled: false,
             imageSmoothingQuality: 'high',
           });
