56c72a215d48b91fcfb642abb9d03318c3b581b7
mahdi <marjani.mahdi@gmail.com>
2021-07-19 20:39:38 +0430

    last faq

--- resources/views/jsonLdFaq.blade.php
+++ resources/views/jsonLdFaq.blade.php
@@ -1,45 +1,55 @@
-@php $i = 0 @endphp
+@php $i = 1;
+$hasFaq=false;
+$jsonLd='';
+@endphp
 
 @foreach($editorModule as $key=>$module)
     @if(ucfirst($module['type']=='faq'))
-        @php
-            $i = 1
-        @endphp
 
-        @if($i==1))
 
-        <script type="application/ld+json">
-                 @php $i = 2 @endphp
-            {
-                         "@context": "https://schema.org",
-						  "@type": "FAQPage","mainEntity":
-						  [
-        @endif
+       @php $hasFaq = true ;@endphp
 
-        @foreach ($module['content'] as $key => $faq)
 
-            {"@type": "Question",
-              "name": "{!! $faq['question'] !!}",
-              "acceptedAnswer":
-              {
-                "@type": "Answer",
-                "text": "{!! $faq['answer'] !!}"
-              }
-            }
+        @foreach ($module['content'] as $faq)
+
+            @php
+            $jsonLd.= PHP_EOL.'{"@type": "Question",';
+
+                $jsonLd.=   '"name": "'.$faq['question'].'",';
+                $jsonLd.= '"acceptedAnswer":
+                    {
+                        "@type": "Answer",
+                        "text": "'.str_replace('"',"'",$faq['answer']) .'"
+                    }
+                }';
+            @endphp
+
+
+            @php $jsonLd.= ','; @endphp
+
 
         @endforeach
 
     @endif
 @endforeach
 
-@if($i==2)
-      ]
-     }
-  </script>
+@if($hasFaq)
+
+@php $jsonLd ='
+            <script type="application/ld+json">
+                {
+                    "@context": "https://schema.org",
+                    "@type": "FAQPage","mainEntity":
+                    [
+
+
+        '.trim($jsonLd,','). '     ]
+     } </script>;';
+
+         @endphp
+
+{!! $jsonLd !!}
 
 @endif
-
-
-
 
 
