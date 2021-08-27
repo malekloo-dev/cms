848171c2d89b7f10da0b8eff2038f7112a990efa
malekloo <mahmoud.malekloo@gmail.com>
2021-08-28 03:37:34 +0430

    breadcrumb

--- resources/views/jsonLdBreadcrumb.blade.php
+++ resources/views/jsonLdBreadcrumb.blade.php
@@ -3,11 +3,19 @@
         "@context": "https://schema.org",
         "@type": "BreadcrumbList",
         "itemListElement": [
+                 {
+                    "@type": "ListItem",
+                    "position": 1,
+                    "item": {
+                        "@id": "{{ url('/') }}",
+                        "name": "{{ env('SITE_NAME','خانه')}}"
+                    }
+                }
             @foreach ($breadcrumb as $index => $item)
 
                 {
                     "@type": "ListItem",
-                    "position": {{ count($breadcrumb) - $index }},
+                    "position": {{ count($breadcrumb) - ($index-1) }},
                     "item": {
                         "@id": "{{ url($item['slug']) }}",
                         "name": "{{ $item['title'] }}"
