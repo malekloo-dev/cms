65ef89d73e08eab733462e75706e536199969e78
mahdi <marjani.mahdi@gmail.com>
2021-11-02 15:39:21 +0330

    fixed remotyadak detail

--- resources/views/remotyadak/cms/Detail.blade.php
+++ resources/views/remotyadak/cms/Detail.blade.php
@@ -192,7 +192,7 @@ height="{{ env('PRODUCT_MEDIUM_W') }}">
 <div>
 <article>
     @if (isset($content->images['images']['small']))
-        <div><img src="{{ $content->images['images']['small'] }}  alt=" {{ $content->title }} "></div>
+        <div><img src="{{ $content->images['images']['small'] }}"  alt="{{ $content->title }}"></div>
 @endif
             <footer>
                 <h2><a href="{{ $content->slug }}"> {{ $content->title }}</a></h2>
