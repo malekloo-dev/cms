72e3453a942e50c75209e79b0cb5114df1fae00e
mahdi <marjani.mahdi@gmail.com>
2022-02-20 17:41:26 +0330

    analytics it times store

--- resources/views/it-times-store/Head.blade.php
+++ resources/views/it-times-store/Head.blade.php
@@ -2,6 +2,18 @@
 <html dir="rtl" lang="fa-IR">
 
 <head>
+    @if (url('/') == 'https://it-times-store.com')
+
+    <!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-PJDSGPV');</script>
+    <!-- End Google Tag Manager -->
+    @endif
+
+
     <title>{{ $seo['meta_title'] ?? ''}}@yield('meta-title')</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
@@ -60,3 +72,9 @@
 </head>
 
 <body>
+    @if (url('/') == 'https://it-times-store.com')
+    <!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PJDSGPV"
+    height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+    <!-- End Google Tag Manager (noscript) -->
+    @endif
