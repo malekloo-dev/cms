ea5b25569c683c1ebb82a7758de177d19188cc3d
mahdi moghadam <marjani.mahdi@gmail.com>
2022-05-05 18:19:59 +0430

    new branch

--- config/filesystems.php
+++ config/filesystems.php
@@ -13,7 +13,8 @@ return [
     |
     */
 
-    'default' => env('FILESYSTEM_DRIVER', 'local'),
+    // 'default' => env('FILESYSTEM_DRIVER', 'local'),
+    'default' => env('FILESYSTEM_DISK', 'local'),
 
     /*
     |--------------------------------------------------------------------------
