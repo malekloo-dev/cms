8814c1c3a620894122e69f348bdcbbaafa40b73d
mahdi moghadam <marjani.mahdi@gmail.com>
2022-05-05 12:26:43 +0430

    added search corepo

--- resources/views/corepo/cms/DetailCategory.blade.php
+++ resources/views/corepo/cms/DetailCategory.blade.php
@@ -85,14 +85,14 @@
 
     </script>
 
-@auth
+    @auth
+        @if (Auth::user()->id == 1)
+            <div class="btn btn-info edit-button"
+                onclick="window.open('{{ url('/admin/category/' . $detail->id . '/edit/') }}')">
+                ویرایش</div>
+        @endif
+    @endauth
 
-@if (Auth::user()->id == 1)
-    <div class="btn btn-info edit-button"
-        onclick="window.open('{{ url('/admin/category/' . $detail->id . '/edit/') }}')">
-        ویرایش</div>
-@endif
-@endauth
 @endsection
 
 @section('Content')
