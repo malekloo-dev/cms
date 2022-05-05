c2f1969242b48be586dde80c6568eb2e0acc97fa
mahdi <marjani.mahdi@gmail.com>
2022-05-05 11:20:20 +0430

    edit button in catgory

--- resources/views/corepo/cms/DetailCategory.blade.php
+++ resources/views/corepo/cms/DetailCategory.blade.php
@@ -84,6 +84,15 @@
         document.querySelector('.next2').addEventListener('click', () => mySiema.next());
 
     </script>
+
+@auth
+
+@if (Auth::user()->id == 1)
+    <div class="btn btn-info edit-button"
+        onclick="window.open('{{ url('/admin/category/' . $detail->id . '/edit/') }}')">
+        ویرایش</div>
+@endif
+@endauth
 @endsection
 
 @section('Content')
