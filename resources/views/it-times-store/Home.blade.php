54c3bf2f4187bb2b26b94039c6ebdfbe2d18e8d8
mahdi moghadam <marjani.mahdi@gmail.com>
2023-02-22 20:26:21 +0330

    it times

--- resources/views/it-times-store/Home.blade.php
+++ resources/views/it-times-store/Home.blade.php
@@ -1,4 +1,4 @@
-@extends(@env('TEMPLATE_NAME').'.App')
+@extends(@env('TEMPLATE_NAME') . '.App')
 
 @push('scripts')
     <script src="{{ asset('/siema.min.js') }}"></script>
@@ -74,10 +74,10 @@
                         <div class="hover text-center">
                             @if (isset($content->images['images']['small']))
                                 <figure class="image">
-                                    <img src="{{ $content->images['images']['small']  }}"
-                                        alt="{{ $content->title }}" width="" height=""
-                                        srcset="{{ $content->images['images']['small']  }} {{ env('CATEGORY_SMALL_W') }}w,
-                                                {{ $content->images['images']['medium']  }} {{ env('CATEGORY_MEDIUM_W') }}w">
+                                    <img src="{{ $content->images['images']['small'] }}" alt="{{ $content->title }}"
+                                        width="" height=""
+                                        srcset="{{ $content->images['images']['small'] }} {{ env('CATEGORY_SMALL_W') }}w,
+                                                {{ $content->images['images']['medium'] }} {{ env('CATEGORY_MEDIUM_W') }}w">
                                     <figcaption>
                                         <div class="p-0 m-0 text-center"> {{ $content->title }}</div>
                                     </figcaption>
@@ -119,23 +119,23 @@
     <div>
         <div class="flex one five-500 center">
             <a href="/سیم-ها" class="flex">
-                <img src="{{ url(env('TEMPLATE_NAME').'/img/سیم.jpg') }}" alt="سیم">
+                <img src="{{ url(env('TEMPLATE_NAME') . '/img/سیم.jpg') }}" alt="سیم">
             </a>
             <a href="/کانکتور-و-تبدیل" class="flex">
-                <img src="{{ url(env('TEMPLATE_NAME').'/img/کانکتور.jpg') }}" alt="کانکتور">
+                <img src="{{ url(env('TEMPLATE_NAME') . '/img/کانکتور.jpg') }}" alt="کانکتور">
             </a>
             <a href="/کابل" class="flex">
-                <img src="{{ url(env('TEMPLATE_NAME').'/img/کابل.jpg') }}" alt="کابل">
+                <img src="{{ url(env('TEMPLATE_NAME') . '/img/کابل.jpg') }}" alt="کابل">
             </a>
             <a href="/تجهیزات-الکترونیک" class="flex">
-                <img src="{{ url(env('TEMPLATE_NAME').'/img/تجهیزات-الکتریکی.jpg') }}" alt="تجهیزات-الکتریکی">
+                <img src="{{ url(env('TEMPLATE_NAME') . '/img/تجهیزات-الکتریکی.jpg') }}" alt="تجهیزات-الکتریکی">
             </a>
         </div>
     </div>
 </section>
 
 <section class="bg-gray-dark my-0 products">
-    <div >
+    <div>
         <h2 class="mb-2">محصولات</h2>
         <div class="flex one five-500">
             {{--product&label=product&var=product&count=20 --}}
@@ -146,9 +146,8 @@
                             <article class="shadow2">
                                 @if (isset($content->images['images']['small']))
                                     <figure class="image">
-                                        <img src="{{ $content->images['images']['small']  }}"
-                                            alt="{{ $content->title }}" width="{{ env('ARTICLE_SMALL_W') }}"
-                                            height="{{ env('ARTICLE_SMALL_H') }}">
+                                        <img src="{{ $content->images['images']['small'] }}" alt="{{ $content->title }}"
+                                            width="{{ env('ARTICLE_SMALL_W') }}" height="{{ env('ARTICLE_SMALL_H') }}">
                                     </figure>
                                 @endif
 
@@ -174,123 +173,125 @@
                         </a>
                     </div>
                 @endforeach
-                @endisset
-            </div>
+            @endisset
+        </div>
     </div>
 
 
-    </section>
+</section>
 
 
-    <section class="home-feature my-5">
-        <div class="flex one two-700">
-            <div class="item">
-                <figure class="third-700">
-                    <img width="128" height="" src="{{ asset('img/1470399662_Marketing.png') }}"  alt="" >
-                </figure>
-                <div class="two-third-700">
-                    <h3>
-                        تنوع بالا محصولات
-                    </h3>
-                    <p>
-                        شما می توانید محصولات ما در دسته بندی های مشخص شده مشاهده نمایید. با ما تماس گرفته و سفارش خود را ثبت نمایید.
-                    </p>
-                </div>
-            </div>
-
-            <div class="item">
-                <figure class="third-700">
-                    <img width="128" height="" src="{{ asset('img/1470399674_App_Development.png') }}"  alt="" >
-                </figure>
-                <div class="two-third-700">
-
-                    <h3>تضمین کیفیت</h3>
-                    <p>محصولات عصر آی تی را می توانید با بالاترین کیفیت به صورت تست شده و یا بدون تست خریداری نمایید.
-                    </p>
-                </div>
-            </div>
-
-            <div class="item">
-                <figure class="third-700">
-                    <img width="128" height="" src="{{ asset('img/1470399671_SEO.png') }}"  alt="" >
-                </figure>
-                <div class="two-third-700">
-
-                    <h3>
-                        ارسال به سراسر ایران
-                    </h3>
-                    <p>
-                        تماس محصولات ما قابلیت ارسال به سراسر ایران را به صورت پست ، تیپاکس و یا پیک دارد.
-                    </p>
-                </div>
-            </div>
-
-            <div class="item">
-                <figure class="third-700">
-                    <img width="128" height="" src="{{ asset('img/1470399667_Newsletter.png') }}"  alt="" >
-                </figure>
-                <div class="two-third-700">
-                    <h3>
-                        مناسب ترین قیمت
-                    </h3>
-                    <p>
-                        فروشگاه ما با مناسب ترین قیمت محصولات الکترونیکی به دلیل عرضه بدون واسطه و مستقیم محصولات می باشد.
-                    </p>
-                </div>
+<section class="home-feature my-5">
+    <div class="flex one two-700">
+        <div class="item">
+            <figure class="third-700">
+                <img width="128" height="" src="{{ asset('img/1470399662_Marketing.png') }}" alt="">
+            </figure>
+            <div class="two-third-700">
+                <h3>
+                    تنوع بالا محصولات
+                </h3>
+                <p>
+                    عصر آی تی امکان تولید و تامین و واردات محصولات خاص مورد نظر شما که در سایت موجود نمی باشد را دارد.
+                </p>
             </div>
         </div>
-    </section>
+
+        <div class="item">
+            <figure class="third-700">
+                <img width="128" height="" src="{{ asset('img/1470399674_App_Development.png') }}"
+                    alt="">
+            </figure>
+            <div class="two-third-700">
+
+                <h3>تضمین کیفیت</h3>
+                <p>
+                    تمامی محصولات عصر آی تی دارای QC و گارانتی اصالت‌و سلامت فیزیکی کالا ‌میباشد
+                </p>
+            </div>
+        </div>
+
+        <div class="item">
+            <figure class="third-700">
+                <img width="128" height="" src="{{ asset('img/1470399671_SEO.png') }}" alt="">
+            </figure>
+            <div class="two-third-700">
+
+                <h3>
+                    ارسال به سراسر ایران
+                </h3>
+                <p>
+                    ارسال به تمامی نقاط ایران با پست پیشتاز ،تیپاکس ،باربری و یا پیک صورت می پذیرد.
+                </p>
+            </div>
+        </div>
+
+        <div class="item">
+            <figure class="third-700">
+                <img width="128" height="" src="{{ asset('img/1470399667_Newsletter.png') }}" alt="">
+            </figure>
+            <div class="two-third-700">
+                <h3>
+                    مناسب ترین قیمت
+                </h3>
+                <p>
+                    با خرید از فروشگاه عصر آی تی بدون واسطه و مستقیم از تولید کننده و وارد کننده، کالای خود را تهیه
+                    فرمایید
+                </p>
+            </div>
+        </div>
+    </div>
+</section>
 
 
 
-    <section class=" bg-gray mb-0 pt-1">
-        {{--post&label=articles&var=articles&count=5--}}
-        <div>
-            <h2>مقالات</h2>
-            <div class="flex five center">
-                @isset($articles['data'])
+<section class=" bg-gray mb-0 pt-1">
+    {{--post&label=articles&var=articles&count=5 --}}
+    <div>
+        <h2>مقالات</h2>
+        <div class="flex one five-500 center">
+            @isset($articles['data'])
                 @foreach ($articles['data'] as $content)
-                <div>
-                    <a href="{{ $content->slug }}">
-                        <article class="shadow2">
-                            @if (isset($content->images['images']['medium']))
-                            <figure class="image">
-                                <img src="{{ $content->images['images']['medium'] }}"
-                                width="198" height="100" alt="{{ $content->title }}">
-                            </figure>
-                            @endif
+                    <div>
+                        <a href="{{ $content->slug }}">
+                            <article class="shadow2">
+                                @if (isset($content->images['images']['medium']))
+                                    <figure class="image">
+                                        <img src="{{ $content->images['images']['medium'] }}" width="198" height="100"
+                                            alt="{{ $content->title }}">
+                                    </figure>
+                                @endif
 
-                            <div class="title">{{ $content->title }}</div>
-                            <div class="info">
-                                {!! readMore($content->brief_description, 250) !!}
-                            </div>
-                            <div class="rate mt-1">
-                                @if (count($content->comments))
-                                @php
-                                        $rateAvrage = $rateSum = 0;
+                                <div class="title">{{ $content->title }}</div>
+                                <div class="info">
+                                    {!! readMore($content->brief_description, 250) !!}
+                                </div>
+                                <div class="rate mt-1">
+                                    @if (count($content->comments))
+                                        @php
+                                            $rateAvrage = $rateSum = 0;
                                         @endphp
-                                    @foreach ($content->comments as $comment)
-                                    @php
-                                            $rateSum = $rateSum + $comment['rate'];
+                                        @foreach ($content->comments as $comment)
+                                            @php
+                                                $rateSum = $rateSum + $comment['rate'];
                                             @endphp
-                                    @endforeach
-                                    @for ($i = $rateSum / count($content->comments); $i >= 1; $i--)
-                                    <img width="20" height="20"
-                                    srcset="{{ asset('/img/star1x.png') }} , {{ asset('/img/star2x.png') }} 2x"
-                                    src="{{ asset('/img/star1x.png') }}"
-                                    alt="{{ 'star for rating' }}">
-                                    @endfor
+                                        @endforeach
+                                        @for ($i = $rateSum / count($content->comments); $i >= 1; $i--)
+                                            <img width="20" height="20"
+                                                srcset="{{ asset('/img/star1x.png') }} , {{ asset('/img/star2x.png') }} 2x"
+                                                src="{{ asset('/img/star1x.png') }}" alt="{{ 'star for rating' }}">
+                                        @endfor
                                     @endif
                                 </div>
 
                             </article>
                         </a>
                     </div>
-                    @endforeach
-                    @endisset
-                </div>
-            </div>
-    </section>
+                @endforeach
+            @endisset
+        </div>
+    </div>
+</section>
 
 
 @endsection
